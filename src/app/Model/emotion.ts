45a6d99a5879d1c2563cf968f9d976a26a62147e
GuauDog <73758776+GuauDog@users.noreply.github.com>
2022-09-09 16:50:53 -0500

    Prueba Finalizada

---
+++ src/app/Model/emotion.ts
@@ -0,0 +1,6 @@
+export interface Emotion {
+    Emotions: {
+        Type: string;
+        Confidence: number;
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {HttpClient,HttpHeaders} from "@angular/common/http"
+import { Observable } from 'rxjs';
+import {Emotion} from "./Model/emotion"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PruebaAngular';
+export class AppComponent implements OnInit{
+  url:string="https://jsonplaceholder.typicode.com/todos/";
+  emotionsArray:any;
+  constructor(private http:HttpClient){
+  }
+
+  ngOnInit(){
+    this.getData().subscribe(data=>{
+      console.log(data);
+      this.emotionsArray=data
+    })
+  }
+
+  getData():Observable<any>{
+    return this.http.get<any>(this.url);
+  }
 }
